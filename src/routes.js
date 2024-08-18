1f2d5fa1da1291de3adec9dc817a11cd00f5cc05
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2024-08-18 14:41:14 -0300

    listando endereços de um usuário

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,10 @@ const AddressController = require('./controllers/AddressController')
 
 const routes = express.Router()
 
-routes.post('/users', UserController.store)
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.store)
 
+routes.get('/users/:user_id/addresses', AddressController.index)
 routes.post('/users/:user_id/addresses', AddressController.store)
 
 module.exports = routes
